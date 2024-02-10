a77492809ad5f50a185e822a273fd6e8cf8af3b1
Natalia Semenova <nataliaberten@gmail.com>
2024-02-10 14:52:09 -0500

    new

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -5,8 +5,8 @@ const ItemModal = ({ selectedCard, onClose }) => {
 
     return (
         <div className={'modal'}>
-        <div className="modal__content">
-        <button type="button" onClick={onClose}> Close </button>  
+        <div className="modal__content-item">
+        <button className='item__button-close' type="button" onClick={onClose}></button>  
           <img src={selectedCard.link} />
           <div>{selectedCard.name}</div>
           <div> Weather type: {selectedCard.weather}  </div>
